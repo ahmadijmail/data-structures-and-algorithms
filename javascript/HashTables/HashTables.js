ea94c78221c50b3ef4a5d1b19400191246080a65
ahmadijmail <ahmadijmil2010@gmail.com>
2022-08-24 08:01:10 +0300

    solved

--- javascript/HashTables/HashTables.js
+++ javascript/HashTables/HashTables.js
@@ -49,10 +49,37 @@ class Hashmap {
 
     return theIndex;
   }
+
+
 }
+
+function findRepeated(string){
+  //console.log("before",string );
+  let lowerCase=string.toLowerCase()
+ // console.log("after",lowerCase );
+ let stringSplit=lowerCase.split(" ")
+  let HashMap= new Hashmap(1000)
+
+  for (let i=0; i<stringSplit.length;i++){
+ 
+//console.log(stringSplit);
+     if(HashMap.contains(stringSplit[i])){
+return {"Repeated Word is:": stringSplit[i]}
+
+
+}
+else  HashMap.set(stringSplit[i], Math.random())
+
+  }return  "No Repeated Words"
+
+} 
 
 //const myhashmap = new Hashmap(100);
 
+let case1= "Hey my name is ahmad My, my name is khaled" 
+let case2= "Hey my name is ahmad My, no repeated word" 
+console.log(findRepeated(case1));
+console.log(findRepeated(case2));
 // myhashmap.hash("shihab", "401d15 student");
 // myhashmap.contains("324", "uhiiuh");
 // myhashmap.set("324", "uhiiuh");
