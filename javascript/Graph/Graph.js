235f0cd7f40eed557511402db5b8e049ef74113e
ahmadijmail <ahmadijmil2010@gmail.com>
2022-09-05 06:50:20 +0300

    breadth first

---
+++ javascript/Graph/Graph.js
@@ -0,0 +1,58 @@
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = [];
+    }
+  }
+
+  addEdge(source, destination) {
+    if (!this.adjacencyList[source]) {
+      this.addVertex(source);
+    }
+    if (!this.adjacencyList[destination]) {
+      this.addVertex(destination);
+    }
+    this.adjacencyList[source].push(destination);
+    this.adjacencyList[destination].push(source);
+  }
+
+  breadthFirst(start) {
+    const queue = [start];
+    const result = [];
+    const visited = {};
+    let currentVertex;
+    visited[start] = true;
+
+    while (queue.length) {
+      currentVertex = queue.shift();
+      console.log("Current Vertex  ", currentVertex);
+      result.push(currentVertex);
+
+      this.adjacencyList[currentVertex].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          queue.push(neighbor);
+        }
+      });
+    }
+    return result;
+  }
+}
+
+let graph = new Graph();
+
+graph.addVertex("1");
+graph.addVertex("2");
+graph.addVertex("3");
+graph.addVertex("4");
+
+graph.addEdge("1", "2");
+graph.addEdge("1", "3");
+graph.addEdge("2", "4");
+graph.addEdge("3", "1");
+
+console.log(graph.breadthFirst("3"));
